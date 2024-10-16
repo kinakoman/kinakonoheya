1b9460b4fd337702174f887eb323128663ff3176
kinakoman <snozaki2017@gmail.com>
2024-10-16 14:51:16 +0900

    commit 20241016-2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Meta from "@/components/Meta";
 import fs from "fs"
+import path from "path"
 
 function name() {
   // for (let i = 0; i < 2; i++) {
@@ -11,11 +12,27 @@ function name() {
   return arr
 }
 
+async function getStaticProps() {
+  const directory = path.join(process.cwd(), 'src', 'app', 'contents')
+  const folders = fs.readdirSync(directory)
+
+  const linkArr = []
+  folders.forEach(element => {
+    linkArr.push(<li><a href={`contents/${element}`}>{`${element}`}</a></li>)
+  });
+
+  return (
+    <ul>
+      {linkArr}
+    </ul>
+  )
+}
+
 export default function Home() {
   return (
     <>
       <Meta title="トップ" />
-      {name()}
+      {getStaticProps()}
 
       <ul>
         <li><a href="contents/new">リンク</a></li>
