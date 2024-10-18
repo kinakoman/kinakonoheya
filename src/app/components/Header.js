e9f6f34069c26adc024279fd3e1c63037a0b4310
kinakoman <snozaki2017@gmail.com>
2024-10-18 18:10:51 +0900

    20241018

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,7 +1,11 @@
-
+import Link from "next/link"
 export default function Header() {
     return <header>
-        <div className="title"><a href="/contents/../">きなこの部屋</a></div>
+        <div className="title">
+            {/* <a href="/contents/../">きなこの部屋</a> */}
+            <Link href="/">きなこの部屋</Link>
+        </div>
+
     </header>
 
 }
