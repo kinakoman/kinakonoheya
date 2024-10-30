ad0a5bcd850a1099e280b469d8136292a7f35f92
PC_of_CXO <snozaki2017@gmail.com>
2024-10-30 13:52:38 +0900

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,9 @@ import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 export const metadata = {
-  icons: [{ rel: 'icon', url: '/icon.png' }]
+  icons: [{ rel: 'icon', url: `${basePath}/icon.png` }]
 };
 
 export default function RootLayout({ children }) {
