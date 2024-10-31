a19fd6bdcaa7d47304dbcadff7747bf40eed3162
PC_of_CXO <snozaki2017@gmail.com>
2024-10-31 20:55:42 +0900

    commit

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,9 +1,17 @@
 import Link from "next/link"
+import Image from "next/image";
+import "@/globals.css"
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const path = `${basePath}/logo.png`
+
 export default function Header() {
     return <header>
         <div className="title">
             {/* <a href="/contents/../">きなこの部屋</a> */}
-            <Link href="/">きなこの部屋</Link>
+            <Link href="/">
+                <Image src={path} alt={"ロゴ画像"} width={400} height={80} style={{ objectFit: 'contain', objectPosition: 'top' }} />
+            </Link>
         </div>
 
     </header>
