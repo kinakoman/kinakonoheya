7ce9319bbd10b0d8687618ff8b3a6202ef40f2d6
PC_of_CXO <snozaki2017@gmail.com>
2024-11-14 16:37:10 +0900

    commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://kinakoman.github.io/kinakonoheya', // GitHub Pages 上の公開URL
+    generateRobotsTxt: true, // robots.txt も生成する場合は true
+};
