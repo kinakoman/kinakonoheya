78d04b5f9e00ae652d3ce531fada8700c8f1b5e9
PC_of_CXO <snozaki2017@gmail.com>
2024-11-21 14:04:38 +0900

    commit

--- src/app/contents/Java/TextBook/page.js
+++ src/app/contents/Java/TextBook/page.js
@@ -322,7 +322,7 @@ public class Sample {
 }`}</Code>
                     <Sub>public,private演算子</Sub>
                     <Tx>public演算子が付いたメンバはクラス外から自由にアクセスが可能です。
-                        private演算子はクラス内部からのアクセスしか受け付けません。
+                        private演算子はクラス内部からのアクセスしか受け付けません。private演算子はサブクラスからのアクセスも不可です。
                     </Tx>
                     <Code lang="java">{`class NewClass {
     public int x = 10;
@@ -361,6 +361,125 @@ public class Sample {
         // 20
     }
 }`}</Code>
+                </Sec>
+                <Sec title="packageとimport">
+                    <Sub>Javaにおけるimport</Sub>
+                    <Tx>Javaでは別のファイルで定義したクラスをimportしてインスタンスを生成することが可能です。
+                        Javaにおけるimportはmainファイルの中でサブファイルで定義したクラスのインスタンスを生成できる状態にすることを指します。
+                    </Tx>
+                    <Sub>package</Sub>
+                    <Tx>packageはクラスのグループを指します。サブファイルの最初にそのファイルが属するpackageを明記します。
+                        ディレクトリ構成が以下の場合のSub1、Sub2のpackageを示します。
+                    </Tx>
+                    <Code lang="shell" tab="ディレクトリ構成">{`main
+├── Main.java
+└── pkg1
+        ├── Sub1.java
+        └── pkg2
+                └── Sub2.java`}</Code>
+                    <Code lang="java" tab="Sub1.java">{`package pkg1;`}</Code>
+                    <Code lang="java" tab="Sub2.java">{`package pkg1.pkg2;`}</Code>
+                    <Sub>import</Sub>
+                    <Tx>サブファイルをメインファイルにインポートする際は、package.ファイル名の形で行います。
+                        同一パッケージからであればインポート無しでインスタンスを生成できます。
+                    </Tx>
+                    <Code lang="java" tab="Main.java">{`import pkg1.Sub1;
+import pkg1.pkg2.Sub2;
+`}</Code>
+                    <Sub>インスタンスの生成</Sub>
+                    <Tx>サブファイルをインポートするとサブファイルで定義したクラスのインスタンスが生成できます。
+                        サブファイルのクラス名は必ずファイル名と一致させます。
+                    </Tx>
+                    <Code lang="java" tab="Sub1.java">{`package pkg1;
+
+public class Sub1 {
+    public void hello() {
+        System.out.println("Hello from Sub1");
+    }
+}`}</Code>
+                    <Code lang="java" tab="Sub2.java">{`package pkg1.pkg2;
+
+public class Sub2 {
+    public void hello() {
+        System.out.println("Hello from Sub2");
+    }
+}`}</Code>
+                    <Code lang="java" tab="Main.java">{`import pkg1.Sub1;
+import pkg1.pkg2.Sub2;
+
+class Main {
+    public static void main(String[] args) {
+        Sub1 ob1 = new Sub1();
+        Sub2 ob2 = new Sub2();
+        ob1.hello();
+        // Hello from Sub1
+        ob2.hello();
+        // Hello from Sub2
+    }
+}`}</Code>
+                    <Sub>アクセス演算子</Sub>
+                    <Tx>public演算子がついたクラス、メンバはインポート先(異なるpackage下)でも自由にアクセス可能です。
+                        インポート先でインスタンスを生成するクラスにはサブファイル側でpublicを指定します。
+                        publicを指定できるクラスは1つのサブファイルで1つまでです。publicが付いたクラスはファイル名と名前が一致する必要があります。
+                        privateは演算子のメンバは自クラスからのみアクセス可能です。
+                    </Tx>
+                    <Code lang="java" tab="Sub1.java">{`package pkg1;
+
+public class Sub1 {
+    private int x;
+    public int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+}`}</Code>
+                    <Code lang="java" tab="Main.java">{`import pkg1.Sub1;
+
+class Main {
+    public static void main(String[] args) {
+        Sub1 ob1 = new Sub1();
+        ob1.y = 10; // publicフィールドはアクセス可能
+        // ob1.x=10; // アクセス不可でエラー
+
+        ob1.setX(10);
+        System.out.println(ob1.getX());
+        // 10
+    }
+}`}</Code>
+                    <Sub>protected演算子</Sub>
+                    <Tx>protected演算子が付いたメンバは自クラスおよびサブクラスからのみアクセスを受け付けます。</Tx>
+                    <Code lang="java" tab="Sub1.java">{`package pkg1;
+
+public class Sub1 {
+    private int x = 10;
+    public int y = 20;
+    protected int z = 30;
+}`}</Code>
+                    <Code lang="java" tab="Main.java">{`import pkg1.Sub1;
+
+class NewSub extends Sub1 {
+    void echo() {
+        // System.out.println(super.x); // アクセス不可でエラー
+        System.out.println(super.y);
+        System.out.println(super.z);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        NewSub ob1 = new NewSub();
+        ob1.echo();
+        // 20
+        // 30
+
+        // ob1.z=100; //protected演算子も外部からのアクセスは不可
+    }
+}`}</Code>
+                    <Tx>同一パッケージ内(同一ファイル内)ではprotected演算子はpublic演算子と同じ扱いです。</Tx>
                 </Sec>
             </Contents>
         </>
